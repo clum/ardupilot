cce86fc06743e6889e9319913dbf617bf6ec5194
Christopher Lum <lum@u.washington.edu>
2018-03-23 15:07:30 -0700

    Making UWSTABILIZE behave like Manual mode

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -783,6 +783,9 @@ void Plane::update_flight_mode(void)
 
     //UWAFSL START
     case UWSTABILIZE: {
+        SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, channel_roll->get_control_in_zero_dz());
+        SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, channel_pitch->get_control_in_zero_dz());
+        steering_control.steering = steering_control.rudder = channel_rudder->get_control_in_zero_dz();
         break;
     }
     //UWAFSL END
